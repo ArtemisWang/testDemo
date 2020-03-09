b049ac4be8ebfc5d4b71e83795525ded9c28bdee
ArtemisWang <xiao2sheng@qq.com>
2020-03-09 14:20:31 +0800

    art

---
+++ demo21.js
@@ -0,0 +1,25 @@
+function throttle(fn, delay=100){
+    let timer=null
+    return function(){
+        if(timer){
+            return
+        }
+        timer=setTimeout(()=>{
+            fn.apply(this, arguments)
+            timer=null
+        },delay)
+    }
+}
+
+function debounce(fn, delay=500){
+    let timer=null
+    return function(){
+        if(timer){
+            clearTimeout(timer)
+        }
+        timer=setTimeout(()=>{
+            fn.apply(this,arguments)
+            timer=null
+        },delay)
+    }
+}
