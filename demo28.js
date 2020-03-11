28cdbafde9b9a2f2f4245fb963dd904382c8d4f5
ArtemisWang <xiao2sheng@qq.com>
2020-03-11 19:24:24 +0800

    art

---
+++ demo28.js
@@ -0,0 +1,45 @@
+function swap(arr, i, j){
+    let temp=arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+}
+
+function checkArray(arr){
+    if(!arr) return
+}
+
+// 归并排序
+function sort(arr){
+    checkArray(arr)
+    mergeSort(arr, 0, arr.length-1)
+    return arr
+}
+
+function mergeSort(arr, left, right){
+    if(left===right) return
+    let mid=left+parseInt((right-left)>>1)
+    mergeSort(arr, left, mid)
+    mergeSort(arr, mid+1, right)
+    
+    let temp=[]
+    let i=0
+    let p1=left
+    let p2=mid+1
+    while(p1<=mid&&p2<=right){
+        temp[i++]=arr[p1]<arr[p2]?arr[p1++]:arr[p2++]
+    }
+    while(p1<=mid){
+        temp[i++]=arr[p1++]
+    }
+    while(p2<=right){
+        temp[i++]=arr[p2++]
+    }
+    for(let j=0;j<temp.length;j++){
+        arr[left++]=temp[j]
+    }
+    return arr
+}
+
+let arr=[1,3,2,8,9,7,6]
+sort(arr)
+console.log(arr)
