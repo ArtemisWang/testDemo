b23d307aa481f5aa462b0488037bf35e84cbcf94
ArtemisWang <xiao2sheng@qq.com>
2020-03-20 15:48:35 +0800

    art

---
+++ demo57.js
@@ -0,0 +1,13 @@
+let i=0
+
+const show=(i)=>{
+    
+    setTimeout(function(){console.log(i)},500)
+}
+
+for(i;i<10;i++){
+    // (function(i){
+    //     console.log(i)
+    // })(i)
+    show(i)
+}
