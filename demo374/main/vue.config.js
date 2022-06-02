7c94e5819bfed77770b162751e6c4b4dfa42893c
yating.wang.ms <yating.wang@faw-vw.com>
2022-06-02 12:21:16 +0800

    20220602

---
+++ demo374/main/vue.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: yating.wang
+ * @Date: 2022-06-01 13:05:40
+ * @LastEditTime: 2022-06-01 13:08:31
+ * @LastEditors: yating.wang
+ * @Description: 
+ */
+
+module.exports = {
+  devServer: {
+    host: '0.0.0.0',
+    port: 8081
+  },
+};
